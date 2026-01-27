0b211703be751f8dbedad39f194f8bb5e0ac3ce3
karnatak95@gmail.com <karnatak95@gmail.com>
2026-01-27 11:13:37 +0530

    added api domain config

--- dreamweaver-backend/main.py
+++ dreamweaver-backend/main.py
@@ -37,6 +37,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:3001",
         "http://127.0.0.1:3001",
+        "https://dreamweaver-s6j9.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
